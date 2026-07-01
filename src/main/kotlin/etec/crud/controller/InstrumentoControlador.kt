3fac41164e5d70f192dca1057d8cc8ed6b28022b
joacopiri <joaquinpirillo5102007@gmail.com>
2026-07-01 11:55:19 -0300

    Practico terminado

--- src/main/kotlin/etec/crud/controller/InstrumentoControlador.kt
+++ src/main/kotlin/etec/crud/controller/InstrumentoControlador.kt
@@ -2,9 +2,9 @@ package etec.crud.controller
 
 import etec.crud.model.Instrumento
 import etec.crud.service.InstrumentoServicio
-import org.apache.coyote.Response
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -13,9 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
 @RequestMapping("api/instrumentos")
-class InstrumentoControlador (
+class InstrumentoControlador(
     @Autowired private val servicio: InstrumentoServicio
 ) {
     @GetMapping
@@ -35,17 +36,35 @@ class InstrumentoControlador (
 
     @PostMapping
     fun crear(@RequestBody instrumento: Instrumento): ResponseEntity<Instrumento> {
-        if (instrumento.nombre.isNotBlank()) {
+        if (instrumento.nombre.isBlank()) {
             return ResponseEntity.badRequest().build()
         }
         val guardado = servicio.guardar(instrumento)
         return ResponseEntity.status(201).body(guardado)
     }
+
     @PutMapping("/{id}")
     fun actualizar(@PathVariable id: Int, @RequestBody instrumento: Instrumento): ResponseEntity<Instrumento> {
-        if(instrumento.nombre.isBlank()) {
+        if (instrumento.nombre.isBlank()) {
             return ResponseEntity.badRequest().build()
+        }
+        val actualizado = servicio.actualizar(id, instrumento)
+        return if (actualizado == null) {
+            ResponseEntity.notFound().build()
+        } else {
+            ResponseEntity.ok(actualizado)
         }
     }
 
+
+    @DeleteMapping("/{id}")
+    fun eliminar(@PathVariable id: Int): ResponseEntity<Void> {
+        val eliminado = servicio.eliminar(id)
+        return if (!eliminado) {
+            ResponseEntity.notFound().build()
+        } else {
+            ResponseEntity.noContent().build()
+        }
+    }
 }
+
--- src/main/kotlin/etec/crud/service/InstrumentoServicio.kt
+++ src/main/kotlin/etec/crud/service/InstrumentoServicio.kt
@@ -1,18 +1,22 @@
 package etec.crud.service
 
 import etec.crud.model.Instrumento
+import org.springframework.stereotype.Service
 
+@Service
 class InstrumentoServicio {
     private val instrumentos = mutableListOf<Instrumento>()
     fun guardar(instrumento: Instrumento): Instrumento {
         instrumentos.add(instrumento)
         return instrumento
     }
+
     fun listarTodos(): List<Instrumento> = instrumentos.toList()
 
     fun buscarPorId(id: Int): Instrumento? {
         return instrumentos.find { it.id == id }
     }
+
     fun actualizar(id: Int, instrumento: Instrumento): Instrumento? {
         val indice = instrumentos.indexOfFirst { it.id == id }
         if (indice == -1) return null
